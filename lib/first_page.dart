cfd46018c2e8516566de759cd79a7eb564db1e80
Md Jamil <mdjamil019240@gmail.com>
2025-10-24 02:26:22 +0600

    Initial commit

---
+++ lib/first_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:test_code_ui/welcome_page.dart';
+ 
+
+class firstpage extends StatefulWidget {
+  const firstpage({super.key});
+
+  @override
+  State<firstpage> createState() => _firstpageState();
+}
+
+class _firstpageState extends State<firstpage> {
+
+
+  void initState() {
+      super.initState();
+
+      Future.delayed(Duration(seconds: 2), () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => WelcomePage()),
+        );
+      });
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 250,
+                width: 250,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/first.png"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
